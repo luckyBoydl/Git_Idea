d3ba4f594f599288adac2bfdc5899f214ecda0ca
影子 <1301536394@qq.com>
2022-07-21 22:25:44 +0800

    Second commit

--- src/main/java/com/itheima/git_test.java
+++ src/main/java/com/itheima/git_test.java
@@ -3,5 +3,6 @@ package com.itheima;
 public class git_test {
     public static void main(String[] args) {
         System.out.println("git_test");
+        System.out.println("git_test2");
     }
 }
